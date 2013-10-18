55ab407a00fc1d22c09c110189fd290768b52f8f
Ivan Doroshenko <ploddi@gmail.com>
2013-10-18 16:44:26 +0400

    Initial Commit

---
+++ CVExtensions/CVExtensions.h
@@ -0,0 +1,13 @@
+//
+//  CVExtensions.h
+//  CVExtensions
+//
+//  Created by Ivan Doroshenko on 10/18/13.
+//  Copyright (c) 2013 DigitalBox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CVExtensions : NSObject
+
+@end
---
+++ CVExtensions/CVExtensions.m
@@ -0,0 +1,13 @@
+//
+//  CVExtensions.m
+//  CVExtensions
+//
+//  Created by Ivan Doroshenko on 10/18/13.
+//  Copyright (c) 2013 DigitalBox. All rights reserved.
+//
+
+#import "CVExtensions.h"
+
+@implementation CVExtensions
+
+@end
